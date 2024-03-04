2c27ecdae26268e042191594c847cea8d637afd6
Cybersparrow Systems <727722eucb058@skcet.ac.in>
2024-03-04 10:29:19 +0530

    Autotyper_Rasaali.py

--- Autotyper_Rasaali.py
+++ Autotyper_Rasaali.py
@@ -6,15 +6,20 @@ import time
 import random
 
 typing_active = False
+paused = False
 
 def type_clipboard():
-    global typing_active
+    global typing_active, paused
     typing_active = True
     text = pyperclip.paste()
     for char in text:
-        pyautogui.typewrite(char)
-        delay = random.uniform(0.001, 0.005)  
-        time.sleep(delay)
+        if not paused:
+            pyautogui.typewrite(char)
+            delay = random.uniform(0.001, 0.005)  
+            time.sleep(delay)
+        else:
+            while paused:
+                time.sleep(0.1)  # Sleeps for a short time to reduce CPU load
         if not typing_active:
             break
     typing_active = False
@@ -31,16 +36,30 @@ def stop_typing():
     global typing_active
     typing_active = False
 
+
+def pause_typing():
+    global paused
+    paused = True
+
+
+def resume_typing():
+    global paused
+    paused = False
+
+
 def on_press(key):
     if key == keyboard.Key.f8:
         start_typing()
     elif key == keyboard.Key.f9:
         stop_typing()
+    elif key == keyboard.Key.f4:
+        if not typing_active:
+            start_typing()
+        elif paused:
+            resume_typing()
+        else:
+            pause_typing()
 
 
 with keyboard.Listener(on_press=on_press) as listener:
     listener.join()
-
-
-
-#ParakumRasaali 
